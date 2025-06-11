8feb353b5b0ba5dd2d0210e5838db0c36965fc5a
Hassan.B <hassan.boucherit@gmail.com>
2025-06-11 14:36:19 +0200

    refacto: revu du backend

--- server.js
+++ server.js
@@ -2,26 +2,23 @@ const express = require('express')
 const config = require('./src/config/config')
 const githubRoutes = require('./src/routes/githubRoutes')
 const groqRoutes = require('./src/routes/groqRoutes')
-const path = require('path');
-
 const cors = require('cors')
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'front/vue')));
 
 app.use(express.json())
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
+
 app.use('/api/github', githubRoutes)
 app.use('/api/groq', groqRoutes)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front/vue/index.html'));
-});
-
+  res.json({ message: 'API de vérification de code' })
+})
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
